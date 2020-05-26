5a43cadb51b3e36db5e34c19e9d41525c9cc07cd
Sara Lablo <sara.labalo18@gmail.com>
2020-05-26 22:41:15 +0200

    pojedinacne stranice

--- src/views/Massages/Massages.js
+++ src/views/Massages/Massages.js
@@ -1,7 +1,89 @@
 import React from "react";
-
+import image from "../../assets/img/masaza.jpg";
+import { FormGroup, Label, Input, FormText, Button } from "reactstrap";
 function Massages() {
-  return <div>Massages</div>;
+  return (
+    <div class="site-section" style={{ marginTop: "100px" }}>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-6">
+            <p class="mb-5">
+              <img src={image} alt="Image" class="img-fluid" />
+            </p>
+          </div>
+          <div class="col-lg-5 ml-auto">
+            <h2 class="site-section-heading mb-3">SPORTSKA MASAŽA</h2>
+            <p>
+              Sportska masaža se preporučuje sportistima kao i rekreativcima.
+              Postoji nekoliko vrsta sportskih masaža. Masaža koja se izvodi
+              nakon treninga ili dugotrajnog napornog rada mišića sprečava
+              nastanak mlečne kiseline, ili istu otklanja ukoliko je došlo do
+              njenog nagomilavanja. Nakupljena mlečna kiselina se pretvara u
+              kalcifikate, koji mogu da izazovu mišićnu napetost pa čak i bol.
+              Postoje i sportske masaže čija je uloga da saniraju najčešće
+              sportske povrede (bacački lakat, teniski lakat, plivačko rame,
+              skakačko koleno itd.). Ova masaža preporučuje se i kada do povrede
+              nije došlo, nego je mišić umoran, pa masažom treba da se opusti i
+              rastereti. Često se rade i sportske masaže koje služe za pripremu
+              sportiste za takmičenje, utakmicu i sl. Ova masaža opušta
+              periferni nervni sistem i priprema telo za nove napore. Sportskim
+              masažama se poboljšava prokrvljenost mišića, pa je on spremniji za
+              napor koji sledi.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="forma">
+        <h2 class="site-section-heading mb-3">Rezervacija termina</h2>
+        <form>
+          <FormGroup>
+            <Label for="firstName">Ime</Label>
+            <Input
+              type="text"
+              name="Name"
+              id="Name"
+              placeholder="Upisite svoje ime"
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label for="lastName">Prezime</Label>
+            <Input
+              type="text"
+              name="lastName"
+              id="lastNamed"
+              placeholder="Upisite svoje Prezime"
+              autoComplete="off"
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label for="exampleEmail">Email</Label>
+            <Input
+              type="email"
+              name="email"
+              id="exampleEmail"
+              placeholder="Enter email"
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label for="date">Datum termina</Label>
+            <Input
+              type="date"
+              name="date"
+              id="date"
+              placeholder="Upisite datum zeljenog termina"
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label for="exampleText">Opis problema</Label>
+            <Input type="textarea" name="text" id="exampleText" />
+          </FormGroup>
+          <Button color="primary" type="submit">
+            Potvrdi
+          </Button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Massages;
--- src/views/Pilates/Pilates.js
+++ src/views/Pilates/Pilates.js
@@ -1,7 +1,90 @@
 import React from "react";
+import image from "../../assets/img/pilates.jpg";
 
 function Pilates() {
-  return <div>Pilates</div>;
+  return (
+    <div class="site-section" style={{ marginTop: "100px" }}>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-6">
+            <p class="mb-5">
+              <img src={image} alt="Image" class="img-fluid" />
+            </p>
+          </div>
+          <div class="col-lg-5 ml-auto">
+            <h2 class="site-section-heading mb-3">PILATES</h2>
+            <p>
+              Trening koji je možda najcelovitiji sinonim fitnesa generalno.
+              Daje rezultate u povećanju elastičnosti, snage i energije,
+              svesnosti sopstvenog tela, poboljšanje mentalne koncentracije.
+              Utiče na poboljšanje ravnoteže i koriguje nepravilno držanje
+              kičme. Preporučuje se početnicima i naprednim rekreativcima.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="border-bottom">
+        <div class="row no-gutters">
+          <div class="col-md-12 col-lg-4">
+            <div class="w-100 h-100 block-feature p-5 bg-light">
+              <h2 class="underline">
+                <span>Stott pilates</span>
+              </h2>
+              <p>
+                STOTT PILATES repertoar čini više od 500 sistematičnih vežbi
+                koje se izvode na strunjači, sa ili bez rekvizita, i na pilates
+                mašinama. To otvara mogućnost lakog modifikovanja i
+                prilagođavanja specifičnim zahtevima svakog pojedinca.
+              </p>
+              <p>
+                <a href="#" class="btn btn-outline-primary py-2 px-4">
+                  Sazanaj više
+                </a>
+              </p>
+            </div>
+          </div>
+          <div class="col-md-12 col-lg-4">
+            <div class="w-100 h-100 block-feature p-5">
+              <h2 class="underline">
+                <span>Klasični pilates</span>
+              </h2>
+              <p>
+                Pilates je inovativni i siguran mind&body program vežbanja koji
+                se uglavnom izvodi na podlozi za vežbanje i uz upotrebu
+                različitih rekvizita. Vežbajući Pilates razvijate svesnost o
+                svom telu, dobru posturu i lake i graciozne pokrete.
+              </p>
+              <p>
+                <a href="#" class="btn btn-outline-primary py-2 px-4">
+                  Sazanaj više
+                </a>
+              </p>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-4">
+            <div class="w-100 h-100 block-feature p-5 bg-light">
+              <h2 class="underline">
+                <span>Performer pilates</span>
+              </h2>
+              <p>
+                Vežbanje na reformeru omogućava aktiviranje velikog broja
+                mišića, celokupne muskulature tela, gde se posebna pažnja
+                poklanja dubokim posturalim mišićima, posebno mobilnosti i
+                stabilnosti kičmenog stuba i dubokih mišića ledja, stabilnosti
+                pelvisa kao i ramenog pojasa.
+              </p>
+              <p>
+                <a href="#" class="btn btn-outline-primary py-2 px-4">
+                  Sazanaj više
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Pilates;
