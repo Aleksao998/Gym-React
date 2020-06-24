6dcaf2d79189a554772cd78ec884f1ae76adfacc
AleksaOpacic <aleksaopacic988@gmail.com>
2020-06-24 12:05:40 +0200

    Updates

---
+++ precache-manifest.b02762a545e6e091f748074ac100d47f.js
@@ -0,0 +1,310 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f4b9d3da15886d98eb21b34a3944daf",
+    "url": "/Gym-React/index.html"
+  },
+  {
+    "revision": "4f279a6c7ef68143280b",
+    "url": "/Gym-React/static/css/main.2e5a89be.chunk.css"
+  },
+  {
+    "revision": "4265ced07838a76bdb6b",
+    "url": "/Gym-React/static/js/2.bcbf5036.chunk.js"
+  },
+  {
+    "revision": "4f279a6c7ef68143280b",
+    "url": "/Gym-React/static/js/main.92821e9d.chunk.js"
+  },
+  {
+    "revision": "746f5cfa2e19128ab863",
+    "url": "/Gym-React/static/js/runtime~main.958f9c31.js"
+  },
+  {
+    "revision": "2ef41f4f29e96e68ff34891f2eca389a",
+    "url": "/Gym-React/static/media/PUMP5.2ef41f4f.jpg"
+  },
+  {
+    "revision": "daa8973d2c21d2e1c1f64185e8520eb1",
+    "url": "/Gym-React/static/media/PUMP6.daa8973d.jpg"
+  },
+  {
+    "revision": "2aa3ae1fbe92dcc28f28643243a705b3",
+    "url": "/Gym-React/static/media/PUMP7.2aa3ae1f.jpg"
+  },
+  {
+    "revision": "5e6f768e7bac2c7d794a01a233cdbf2f",
+    "url": "/Gym-React/static/media/PUMP8.5e6f768e.jpg"
+  },
+  {
+    "revision": "85b5fd8bbe4e4228aff83b897bfd581c",
+    "url": "/Gym-React/static/media/aJoga1.85b5fd8b.jpg"
+  },
+  {
+    "revision": "c2fe8a9b7c5b7274c852f4208e5f5816",
+    "url": "/Gym-React/static/media/aJoga2.c2fe8a9b.jpg"
+  },
+  {
+    "revision": "57fa94732953e67f2e89a5e3e08c83ab",
+    "url": "/Gym-React/static/media/aJoga3.57fa9473.jpg"
+  },
+  {
+    "revision": "dcd0d1aa29721ea483814d18b1091fd1",
+    "url": "/Gym-React/static/media/aJoga4.dcd0d1aa.jpg"
+  },
+  {
+    "revision": "19895e20b4f8967c357c0219fb4687bd",
+    "url": "/Gym-React/static/media/astanga.19895e20.jpg"
+  },
+  {
+    "revision": "75fcd33ae586d758a4fa6430009855e2",
+    "url": "/Gym-React/static/media/bahkti.75fcd33a.jpg"
+  },
+  {
+    "revision": "988a09235d828276a4a9d6374cbb796f",
+    "url": "/Gym-React/static/media/bakram2.988a0923.jpg"
+  },
+  {
+    "revision": "51ecfce0ca3a1b3d2937fb6e98c42fed",
+    "url": "/Gym-React/static/media/bakram3.51ecfce0.jpg"
+  },
+  {
+    "revision": "16222fa42061906930bbbfaf7703aff9",
+    "url": "/Gym-React/static/media/bakram4.16222fa4.jpg"
+  },
+  {
+    "revision": "71f579bc95cda7569db427eac87a56fe",
+    "url": "/Gym-React/static/media/bikram.71f579bc.jpg"
+  },
+  {
+    "revision": "b177a5b8aef6300a606be21d51e344ea",
+    "url": "/Gym-React/static/media/cardio.b177a5b8.jpg"
+  },
+  {
+    "revision": "38ca5ee32e7fd2ae1e4d49f40ae35997",
+    "url": "/Gym-React/static/media/core-30.38ca5ee3.jpg"
+  },
+  {
+    "revision": "2ebe89008a279deaae9a52a2545ef9b7",
+    "url": "/Gym-React/static/media/core.2ebe8900.jpg"
+  },
+  {
+    "revision": "11086c7ecd1000258b431ef5fd6598e7",
+    "url": "/Gym-React/static/media/core301.11086c7e.jpg"
+  },
+  {
+    "revision": "a648b73951c68ebedd06735a263c68c8",
+    "url": "/Gym-React/static/media/core302.a648b739.jpg"
+  },
+  {
+    "revision": "1031912836a610fbe694731dc420b02f",
+    "url": "/Gym-React/static/media/core303.10319128.jpg"
+  },
+  {
+    "revision": "96dc0b0cb05a0427f9d3ef4a2ae61334",
+    "url": "/Gym-React/static/media/core304.96dc0b0c.png"
+  },
+  {
+    "revision": "ed5c739132a77abe4eb51c501e8d6298",
+    "url": "/Gym-React/static/media/force.ed5c7391.jpg"
+  },
+  {
+    "revision": "b7ad25994ff4d22c5a1431ecedbab5e1",
+    "url": "/Gym-React/static/media/heroImage.b7ad2599.jpg"
+  },
+  {
+    "revision": "5e1a4715441937058039452d9091fe62",
+    "url": "/Gym-React/static/media/img_1.5e1a4715.jpg"
+  },
+  {
+    "revision": "adabb24145fcf3f3e0651239fc550333",
+    "url": "/Gym-React/static/media/joga1.adabb241.jpg"
+  },
+  {
+    "revision": "e99aa33f9c443ea63187a73922a4d627",
+    "url": "/Gym-React/static/media/joga2.e99aa33f.jpg"
+  },
+  {
+    "revision": "015c202f4a51d331ab96e4aa45968aab",
+    "url": "/Gym-React/static/media/joga3.015c202f.jpg"
+  },
+  {
+    "revision": "d5d65ad7cf27a72e2240b6777907e3ca",
+    "url": "/Gym-React/static/media/joga4.d5d65ad7.jpg"
+  },
+  {
+    "revision": "b1c775c67c923b7ba98aaa487e2a23a3",
+    "url": "/Gym-React/static/media/klasicniP.b1c775c6.jpg"
+  },
+  {
+    "revision": "2d413bbba1b0594f1461c3728c6a98ef",
+    "url": "/Gym-React/static/media/masaza.2d413bbb.jpg"
+  },
+  {
+    "revision": "48ac036da7c7923266266f63746fc5f1",
+    "url": "/Gym-React/static/media/nucleo-icons.48ac036d.ttf"
+  },
+  {
+    "revision": "5c78b4cf03771fd2287325c44f0d3cd9",
+    "url": "/Gym-React/static/media/nucleo-icons.5c78b4cf.woff2"
+  },
+  {
+    "revision": "6836b93bf1af0b571928d98458a68445",
+    "url": "/Gym-React/static/media/nucleo-icons.6836b93b.woff"
+  },
+  {
+    "revision": "f32ed5ad3d76067b8e43be1a0bd1ba91",
+    "url": "/Gym-React/static/media/nucleo-icons.f32ed5ad.eot"
+  },
+  {
+    "revision": "c5bd5784808e63d768300908563cb07c",
+    "url": "/Gym-React/static/media/nutrition.c5bd5784.jpg"
+  },
+  {
+    "revision": "9c381b1dedd0013df17efe21b20a30c5",
+    "url": "/Gym-React/static/media/performer.9c381b1d.jpg"
+  },
+  {
+    "revision": "6844ae7f3854fd66f9b03562435e4056",
+    "url": "/Gym-React/static/media/performer1.6844ae7f.jpg"
+  },
+  {
+    "revision": "bd543909026eb911f1550daeb7847482",
+    "url": "/Gym-React/static/media/performer2.bd543909.jpg"
+  },
+  {
+    "revision": "77b59d02c960c07133adaa918e61f3b1",
+    "url": "/Gym-React/static/media/performer3.77b59d02.jpg"
+  },
+  {
+    "revision": "9b9ceb21b8dcba29d0de3939f85bf9bd",
+    "url": "/Gym-React/static/media/performer4.9b9ceb21.jpg"
+  },
+  {
+    "revision": "709fa7cd02f9f9c1a4f3993dc1bc0c2d",
+    "url": "/Gym-React/static/media/person_1.709fa7cd.jpg"
+  },
+  {
+    "revision": "545486c358d7992328c4b1b8feb59433",
+    "url": "/Gym-React/static/media/person_2.545486c3.jpg"
+  },
+  {
+    "revision": "5b8a8c009479b09f53cf7a7192cb7199",
+    "url": "/Gym-React/static/media/person_3.5b8a8c00.jpg"
+  },
+  {
+    "revision": "ccd8da51b1c9f317975f0a5ef3273196",
+    "url": "/Gym-React/static/media/person_4.ccd8da51.jpg"
+  },
+  {
+    "revision": "a2d5e7b8ea676324e713c07ebc6a929b",
+    "url": "/Gym-React/static/media/pilates.a2d5e7b8.jpg"
+  },
+  {
+    "revision": "fe911796db87684caffa72a787f8b863",
+    "url": "/Gym-React/static/media/pilates1.fe911796.jpg"
+  },
+  {
+    "revision": "4bd19b43df12d0025731b9980d5181d5",
+    "url": "/Gym-React/static/media/pilates2.4bd19b43.jpg"
+  },
+  {
+    "revision": "8d09ec0e85da45e20e4b02fe83c3a920",
+    "url": "/Gym-React/static/media/pilates3.8d09ec0e.jpg"
+  },
+  {
+    "revision": "0ae12ad3159cc5d6fc843833b6ec2f5d",
+    "url": "/Gym-React/static/media/pilates4.0ae12ad3.jpg"
+  },
+  {
+    "revision": "042287067eb8d0f0b634c12ea88827b0",
+    "url": "/Gym-React/static/media/pump.04228706.jpg"
+  },
+  {
+    "revision": "47081071d0904006468bc6bd93a379c7",
+    "url": "/Gym-React/static/media/pump1.47081071.jpg"
+  },
+  {
+    "revision": "dfad01438bd3772e3331bb83fb0224ba",
+    "url": "/Gym-React/static/media/pump2.dfad0143.jpg"
+  },
+  {
+    "revision": "ee3955c4c2e0e11ff03fa9043b82c9b3",
+    "url": "/Gym-React/static/media/running1.ee3955c4.png"
+  },
+  {
+    "revision": "e42f325549d40380b53b201b8fd073a7",
+    "url": "/Gym-React/static/media/running2.e42f3255.jpg"
+  },
+  {
+    "revision": "9af84003bd12acbd11ef6018046cbc49",
+    "url": "/Gym-React/static/media/running3.9af84003.jpg"
+  },
+  {
+    "revision": "2b71ed627e15f2c6948c6d6eb7650fbf",
+    "url": "/Gym-React/static/media/running4.2b71ed62.jpg"
+  },
+  {
+    "revision": "7ea1ec8f0aef1f1e2fa53913c5172bc3",
+    "url": "/Gym-React/static/media/startit.7ea1ec8f.jpg"
+  },
+  {
+    "revision": "2f9917caacebc44868c96473ca7e16a0",
+    "url": "/Gym-React/static/media/startit1.2f9917ca.jpg"
+  },
+  {
+    "revision": "6b4ce28ab32422bc3a21583301884da4",
+    "url": "/Gym-React/static/media/startit2.6b4ce28a.jpg"
+  },
+  {
+    "revision": "face9c34f58306f6484859a310e0daee",
+    "url": "/Gym-React/static/media/startit3.face9c34.jpg"
+  },
+  {
+    "revision": "1de027121c2d473f468d339f32689311",
+    "url": "/Gym-React/static/media/startit4.1de02712.jpg"
+  },
+  {
+    "revision": "59a04e8b756cd5ba05dc32040795a1db",
+    "url": "/Gym-React/static/media/startits.59a04e8b.jpg"
+  },
+  {
+    "revision": "58b0aec31931a1190e1910f40449c244",
+    "url": "/Gym-React/static/media/step1.58b0aec3.jpg"
+  },
+  {
+    "revision": "3cb9094e22eed84e2d43967cc6b8ca04",
+    "url": "/Gym-React/static/media/step2.3cb9094e.jpg"
+  },
+  {
+    "revision": "1385a56c93cdf74de0f5f789e70b37d0",
+    "url": "/Gym-React/static/media/step3.1385a56c.jpg"
+  },
+  {
+    "revision": "e23855b61282cedd72d40883d64aa376",
+    "url": "/Gym-React/static/media/step4.e23855b6.jpg"
+  },
+  {
+    "revision": "69a44f3cafc2e520bdb0df1ef976b29e",
+    "url": "/Gym-React/static/media/stott1.69a44f3c.jpg"
+  },
+  {
+    "revision": "2c26a811a2b3205f81fd2633c6f60f6e",
+    "url": "/Gym-React/static/media/stott2.2c26a811.jpg"
+  },
+  {
+    "revision": "d2fa7963b4cae2010fdf167ea7c6e3d5",
+    "url": "/Gym-React/static/media/stott3.d2fa7963.jpg"
+  },
+  {
+    "revision": "afb37fa60398aa2b24136d512b2e3813",
+    "url": "/Gym-React/static/media/stott4.afb37fa6.jpg"
+  },
+  {
+    "revision": "48370f2323d936120bf65cde09b51349",
+    "url": "/Gym-React/static/media/trcanje.48370f23.jpg"
+  },
+  {
+    "revision": "56de5240f17205e4d3f51b8ab9144ab8",
+    "url": "/Gym-React/static/media/yoga.56de5240.jpg"
+  }
+]);
